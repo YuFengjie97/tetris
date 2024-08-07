4a83484557e10da4e44bebe24e22aa4f8f530f08
yufengjie <2457522528@qq.com>
2024-08-07 17:53:59 +0800

    next tetromino tip

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,27 +4,58 @@ extends Node
 @onready var score = $Score
 @onready var level = $Level
 @onready var lines = $Lines
+@onready var tetromino_tip_next_1 = $TetrominoTipNext1
+@onready var tetromino_tip_next_2 = $TetrominoTipNext2
+@onready var tetromino_tip_next_3 = $TetrominoTipNext3
+@onready var tetromino_tip_hold = $TetrominoTipHold
+
 
 var tetrominos: Array[Tetromino] = []
 var current_tetromino: Tetromino
+var current_tetromino_type: Global.Tetromino = Global.Tetromino.values().pick_random()
+var next_tetrominos_type: Array[Global.Tetromino] = []
+var hold_tetromino_type: Global.Tetromino
+
 
 
 const tetromino_scene = preload("res://scenes/tetromino.tscn")
 
 func _ready():
 	create_tetromino()
+	init_next_tetrominos_type()
+
+
+func init_next_tetrominos_type():
+	for i in range(3):
+		var type = Global.Tetromino.values().pick_random()
+		next_tetrominos_type.append(type)
+	update_tetromino_tip()
+
+
+func update_tetrominos_type():
+	current_tetromino_type = next_tetrominos_type[0]
+	var new_type = Global.Tetromino.values().pick_random()
+	next_tetrominos_type.remove_at(0)
+	next_tetrominos_type.append(new_type)
+	update_tetromino_tip()
+
+
+func update_tetromino_tip():
+	tetromino_tip_next_1.update_type(next_tetrominos_type[0])
+	tetromino_tip_next_2.update_type(next_tetrominos_type[1])
+	tetromino_tip_next_3.update_type(next_tetrominos_type[2])
 
 
 func create_tetromino():
-	var current_type = Global.Tetromino.values().pick_random()
 	current_tetromino = tetromino_scene.instantiate()
 	add_child(current_tetromino)
 	current_tetromino.other_tetrominos = tetrominos
 	current_tetromino.locked.connect(on_tetromino_locked)
-	current_tetromino.create(current_type)
+	current_tetromino.create(current_tetromino_type)
 
 
 func on_tetromino_locked():
 	print('game get tetromino locked')
 	tetrominos.append(current_tetromino)
+	update_tetrominos_type()
 	create_tetromino()
---
+++ scripts/tetromino_tip.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+@export var type: Global.Tetromino = Global.Tetromino.I
+@export var tetromino_scale = 0.5
+
+var piece_scene = preload("res://scenes/piece.tscn")
+var pieces: Array[Piece] = []
+
+func _ready():
+	update_type(Global.Tetromino.I)
+
+
+func update_type(tetromino_type: Global.Tetromino):
+	var childs = get_children()
+	for child in childs:
+		child.queue_free()
+	type = tetromino_type
+	var data = Global.data[type]
+	var texture = data.texture
+	var piece_coords = Global.piece_coords[type]
+	for piece_coord in piece_coords:
+		var piece = piece_scene.instantiate() as Piece
+		pieces.append(piece)
+		add_child(piece)
+		piece.set_texture(texture)
+		piece.position = piece_coord * Global.grid_size
+
